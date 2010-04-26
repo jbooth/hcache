84431c1b9e0b3adb38bab556a178dcfd12f372e0
Jay Booth <jaybooth@gmail.com>
2010-04-25 23:23:22 -0400

    added StringReader utility

---
+++ src/main/java/hcache/io/StringReader.java
@@ -0,0 +1,30 @@
+package hcache.io;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+
+/**
+ * Composes a CdbReader<Text,Text> and provides fast utilities for 
+ * String queries by maintaining a static threadlocal Text as a buffer.
+ */
+public class StringReader implements Closeable {
+  private static ThreadLocal<Text> keyBuff = new ThreadLocal<Text>();
+  private static ThreadLocal<Text> valBuff = new ThreadLocal<Text>();
+  private CdbReader<Text,Text> in;
+  
+  public StringReader(CdbReader<Text,Text> in) {
+    this.in=in;
+  }
+  
+  public String get(String key) throws IOException {
+    keyBuff.get().set(key);
+    in.find(keyBuff.get(), valBuff.get());
+    return valBuff.toString();
+  }
+  
+  public void close() throws IOException {
+    in.close();
+  }
+}
